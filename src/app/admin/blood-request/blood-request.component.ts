d103a50c40030a7fd25ab3d1e6421739a2706228
sridharshankar25 <sridhar25600@gmail.com>
2022-06-23 16:04:38 +0530

    Bloodreq admin update

--- src/app/admin/blood-request/blood-request.component.ts
+++ src/app/admin/blood-request/blood-request.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { bloodGroup } from 'src/app/interfaces/bloodGroup';
+import { bloodRequest } from 'src/app/interfaces/bloodRequest';
+import { BloodrequestService } from 'src/app/service/bloodRequest/bloodrequest.service';
 
 @Component({
   selector: 'app-blood-request',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BloodRequestComponent implements OnInit {
 
-  constructor() { }
+  bloodreq : bloodRequest[] = [];
+  constructor(private bloodReqService : BloodrequestService , private toastr : ToastrService) { }
 
   ngOnInit(): void {
+    this.bloodReqService.allBloodRequests().subscribe((res)=>{
+      this.bloodreq = res;
+      console.log(this.bloodreq);
+    },(err)=>{
+      console.log("Error fetching details");
+    })
   }
 
+  deleteRequest(res : bloodRequest){
+    this.bloodReqService.deleteBloodRequest(res._id).subscribe((res)=>{
+      this.toastr.error("Request Deleted !");
+      this.bloodReqService.allBloodRequests().subscribe((res)=>{
+        this.bloodreq = res;
+      })
+    },(err)=>{
+      console.log("Error fetching details");
+    })
+  }
 }
--- src/app/interfaces/bloodRequest.ts
+++ src/app/interfaces/bloodRequest.ts
@@ -2,11 +2,13 @@ import { bloodGroup } from "./bloodGroup";
 
 
 export interface bloodRequest {
+    _id : string;
     name: string;
     email: string;
     bloodType: bloodGroup;
     contact: string;
     address: string;
+    requestDate :Date;
     createdAt: Date;
     updatedAt: Date;
 }
