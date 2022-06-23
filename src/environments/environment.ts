c87a6d396a3908777d4e27e32e25d02717326d90
PREMSR0202 <srprem2000@gmail.com>
2022-06-23 10:37:57 +0530

    Enviroment API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,12 @@
 
 export const environment = {
   production: false,
-  api : 'http://localhost:8080/',
+  api: 'http://blooddonationapp-env.eba-bjdtpx52.us-east-1.elasticbeanstalk.com/',
+  // api : 'http://localhost:8080/',
   gapi: 'AIzaSyDCYx_gzFFPyt0dGPSNNHfsUPhsx78GiMo',
   cognito: {
     userPoolId: 'us-east-1_Hcw97y3OW',
-    userPoolWebClientId: '5konrl9mos6vtcftf3aa0iralu'    
+    userPoolWebClientId: '5konrl9mos6vtcftf3aa0iralu'
   },
   region: 'us-east-1',
   accessKeyId: 'AKIA5A455FMMVTBCQX2R',
