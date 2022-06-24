01ba6a98b9b80f3c3d1e6c6f4b42098b5f009ef9
Prem S R <64535689+PREMSR0202@users.noreply.github.com>
2022-06-24 18:15:24 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   production: true,
   api: 'http://blooddonationapp-env.eba-bjdtpx52.us-east-1.elasticbeanstalk.com/',
   // api : 'http://localhost:8080/',
-  gapi: 'AIzaSyDCYx_gzFFPyt0dGPSNNHfsUPhsx78GiMo',
   cognito: {
     userPoolId: 'us-east-1_Hcw97y3OW',
     userPoolWebClientId: '5konrl9mos6vtcftf3aa0iralu'
